fa46b38a4a7ccda97a7b2b4d0036c864a88d60cb
mehedi <mdmehedi121888@gmail.com>
2025-01-04 15:19:12 +0600

    accrodion component added

---
+++ app/accordion/page.tsx
@@ -0,0 +1,39 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
+  export default function AccordionDemo() {
+    return (
+        <>
+        <h1 className="text-2xl text-center text-3xl font-bold my-10">Accordion section </h1>
+      <div className="w-[50%] mx-auto my-10 p-4 space-y-4 bg-gray-100 dark:bg-gray-800 space-x-4">
+        <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      </div></>
+       
+    )
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Page</h1>
+      <h1 className="p-5 text-3xl text-[#fb923c]">This is Home Page</h1>
     </div>
   );
 }
