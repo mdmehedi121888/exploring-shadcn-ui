dd38d294ee2fc69a528e3d4c86896f7371211e25
mehedi <mdmehedi121888@gmail.com>
2025-01-04 16:10:49 +0600

    alert dialog added

--- app/alert/page.tsx
+++ app/alert/page.tsx
@@ -1,10 +1,23 @@
-import { AlertCircle, Terminal } from "lucide-react"
+import { AlertCircle, SquareX, StepForward, Terminal } from "lucide-react"
  
 import {
   Alert,
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+  import { Button } from "@/components/ui/button"
  
   export default function Alerts() {
     return (
@@ -26,7 +39,31 @@ import {
         Your session has expired. Please log in again.
       </AlertDescription>
     </Alert>
-      </div></>
+      </div>
+
+      <h1 className="text-2xl text-center text-3xl font-bold my-10">Alert Dialog section </h1>
+      <div className="w-[50%] mx-auto my-10 p-4 space-y-4 bg-gray-100 dark:bg-gray-800 space-x-4">
+      <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel><SquareX /></AlertDialogCancel>
+          <AlertDialogAction><StepForward /></AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+      </div>
+      
+      </>
        
     )
   }
